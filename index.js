e034cdc3f03574240bd625e3bb7ba3438239b66f
aggre <hiroyuki.aggre@gmail.com>
2018-10-31 15:45:42 +0900

    Update from Qiita

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var express = require( 'express' )
-var app = express()
-var path = require( 'path' )
+const express = require('express')
+const { join } = require('path')
+const app = express()
 
-app.get( '/*', function( req, res ) {
-    res.sendFile( path.join( __dirname + '/index.html' ) )
-} )
+app.get('/*', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'))
+})
 
 app.listen()
